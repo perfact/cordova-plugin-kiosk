6af176d3c37fa4b334f1c7d541cb2eac6ee1596d
Alexander Rolfes <9316893+Wiseqube@users.noreply.github.com>
2024-05-24 16:01:49 +0200

    Remove statusbar overlay (#1)
    * Remove StatusBarOverlay.java
    
    * Remove usage in KioskActivity
    
    * More removals

--- android/KioskActivity.java
+++ android/KioskActivity.java
@@ -24,8 +24,6 @@ public class KioskActivity extends CordovaActivity {
     public static volatile boolean running = false;
     public static volatile Set<Integer> allowedKeys = Collections.EMPTY_SET;
 
-    private StatusBarOverlay statusBarOverlay = null;
-
     @Override
     protected void onStart() {
         super.onStart();
@@ -64,17 +62,11 @@ public class KioskActivity extends CordovaActivity {
         ActionBar actionBar = getActionBar();
         if (actionBar != null) actionBar.hide();
         
-        // add overlay to prevent statusbar access by swiping
-        statusBarOverlay = StatusBarOverlay.createOrObtainPermission(this);
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
-        if (statusBarOverlay != null) {
-            statusBarOverlay.destroy(this);
-            statusBarOverlay = null;
-        }
     }
 
     @Override
